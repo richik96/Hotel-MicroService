ae564dca5ce051bad99ce3fb002b454f716e9901
richik96 <drichik@gmail.com>
2025-02-08 21:43:42 +0530

    adding zipkin through docker

---
+++ apigatewayjan25/src/test/java/org/ecomerce/apigatewayjan25/ApiGateWayJan25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecomerce.apigatewayjan25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayJan25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- service-registry-jan25/src/main/java/org/ecomerce/serviceregistryjan25/ServiceRegistryJan25Application.java
+++ service-registry-jan25/src/main/java/org/ecomerce/serviceregistryjan25/ServiceRegistryJan25Application.java
@@ -2,7 +2,6 @@ package org.ecomerce.serviceregistryjan25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
